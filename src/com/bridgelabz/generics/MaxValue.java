b113d93f7658ace4e9669bc9044a427da470b766
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-25 23:41:18 +0530

    Modified: max integer of 3values

--- src/com/bridgelabz/generics/MaxValue.java
+++ src/com/bridgelabz/generics/MaxValue.java
@@ -4,6 +4,7 @@ public class MaxValue {
     void maxValue(Integer val1,Integer val2,Integer val3){
         if((val1.compareTo(val2))>=0 && (val1.compareTo(val3))>=0)
             System.out.println(val1+ " is the largest");
+
             else if ((val2.compareTo(val1))>=0 && (val2.compareTo(val3))>=0) {
                 System.out.println(val2+" is largest");
             }
