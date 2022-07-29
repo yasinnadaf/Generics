58e1a89ef6fac47382119c76cc8235fab239e4cf
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-29 19:59:42 +0530

    Modified: given 3Floats of maximum

--- src/com/bridgelabz/generics/MainClass.java
+++ src/com/bridgelabz/generics/MainClass.java
@@ -3,13 +3,13 @@ package com.bridgelabz.generics;
 public class MainClass {
     public static void main(String[] args) {
 
-        MaxValue floatmax = new MaxValue();
+        MaxValue floatMax = new MaxValue();
         System.out.println("Test case 1");
-        floatmax.maxValue(2.3F,5.0F,3.3F);
+        floatMax.maxValue(2.3F,5.0F,3.3F);
         System.out.println("Test case 2");
-        floatmax.maxValue(1.0F,8.9F,4.0F);
+        floatMax.maxValue(1.0F,8.9F,4.0F);
         System.out.println("Test case 3");
-        floatmax.maxValue(7.0F,1.5F,8.0F);
+        floatMax.maxValue(7.0F,1.5F,8.0F);
 
     }
 }
