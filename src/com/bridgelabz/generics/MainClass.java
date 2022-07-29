b4c60a0fc035ddc32544ea787333f1619db741cb
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-29 20:15:15 +0530

    Modified: maximum of three strings

--- src/com/bridgelabz/generics/MainClass.java
+++ src/com/bridgelabz/generics/MainClass.java
@@ -3,13 +3,13 @@ package com.bridgelabz.generics;
 public class MainClass {
     public static void main(String[] args) {
 
-        MaxValue floatMax = new MaxValue();
+        MaxValue stringMax = new MaxValue();
         System.out.println("Test case 1");
-        floatMax.maxValue(2.3F,5.0F,3.3F);
+        stringMax.maxValue("Apple","Peach","Banana");
         System.out.println("Test case 2");
-        floatMax.maxValue(1.0F,8.9F,4.0F);
+        stringMax.maxValue("Peach","Banana","Apple");
         System.out.println("Test case 3");
-        floatMax.maxValue(7.0F,1.5F,8.0F);
+        stringMax.maxValue("Banana","Apple","Peach");
 
     }
 }
